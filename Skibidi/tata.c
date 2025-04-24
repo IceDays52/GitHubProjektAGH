e494bcfa18d2191a0845202de5029fb916e23c8b
IceDays52 <icedays222@gmail.com>
2025-04-24 20:04:15 +0200

    poczatek projektu

--- Skibidi/tata.c
+++ Skibidi/tata.c
@@ -1,1 +1,16 @@
-	dasdadadasdasdada
+#include <stdio.h>
+#include <string.h>
+
+struct Posilek {
+	char nazwa_posilku[100];
+	int kcal;
+	float bialko;
+	float tluszcz;
+	float wegle;
+};
+struct Posilek sniadania[] = {
+	{"Owsianka z owocami", 350, 10.5, 8.2, 55.0},
+	{"Jajecznica z warzywami", 300, 20.0, 22.0, 5.0},
+	{"Jogurt naturalny z musli", 320, 15.0, 10.0, 40.0}
+};
+
