d98e8bf363f5b31460eab188269d015c20cf462e
Mateusz Turakiewicz <nienawidzeczarnoskorych@MacBook-Air-Mateusz.local>
2025-04-25 19:20:52 +0200

    test na tym branchu - aktualizacja gtk

--- analiza_wejsciowych.c
+++ analiza_wejsciowych.c
@@ -1,1 +1,16 @@
-#include stdio.h
+#include <stdio.h>
+#include <stdlib.h>
+#include <gtk/gtk.h>
+
+// Callback wywoływany po kliknięciu przycisku
+void on_button_clicked(GtkWidget *widget, gpointer data) {
+    GtkEntry *entry = GTK_ENTRY(data);
+    const gchar *text = gtk_entry_get_text(entry);
+    g_print("Wprowadzono: %s\n", text);
+}
+
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+
+
+}
